35cf5fc9bd328a1e8471025db4cfd0c69c6f2563
ridhwan <muhammadridhwandaud@gmail.com>
2017-07-24 10:30:00 +0800

    add employeesList reducer

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {	
-	EMPLOYEE_UPDATE	
+	EMPLOYEE_UPDATE,
+	EMPLOYEE_CREATE,
+	EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -16,10 +18,25 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
 	const { currentUser } = firebase.auth();
 
-	return () => {
+	return (dispatch) => {
 		firebase.database().ref(`/users/${currentUser.uid}/employees`)
 			.push({ name, phone, shift })
-			.then(() => Actions.employeeList({ type: 'reset' }));
+			.then(() => { 
+				dispatch({ type:EMPLOYEE_CREATE })
+				Actions.employeeList({ type: 'reset' }) 
+			});
 	}	
 
+};
+
+//get list from firebase
+export const employeesFetch = () => {
+	const { currentUser } = firebase.auth();
+
+	return(dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.on('value',snapshot => {
+				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+			});
+	};
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Text, View } from 'react-native';
+import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
+
+	componentWillMount() {
+		this.props.employeesFetch();
+	}
+
 	render(){
 		return(
 			<View>
@@ -15,4 +22,4 @@ class EmployeeList extends Component {
 	}
 }
 
-export default EmployeeList;
+export default connect(null, { employeeFetch }) (EmployeeList);
