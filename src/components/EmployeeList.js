2c6ed7562872539a516d315afbfb861c4965472d
ridhwan <muhammadridhwandaud@gmail.com>
2017-07-24 11:12:59 +0800

    create datasource

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
-import { employeeFetch } from '../actions';
+import { ListView, Text, View } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
 
 	componentWillMount() {
 		this.props.employeesFetch();
+
+		this.createDataSource(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.createDataSource(nextProps);
+	}
+
+	createDataSource({ employees })	{
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1,r2) => r1 !== r2
+		});
+
+		this.dataSource = ds.cloneWithRows(employees);
 	}
 
 	render(){
+		console.log(this.props);
 		return(
 			<View>
 				<Text>Employee List</Text>
@@ -22,4 +38,12 @@ class EmployeeList extends Component {
 	}
 }
 
-export default connect(null, { employeeFetch }) (EmployeeList);
+const mapStateToProps = state => {
+	const employees = _.map(state.employees, (val, uid) => {
+		return { ...val, uid};
+	});
+
+	return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch }) (EmployeeList);
